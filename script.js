092dcd7d1837ea66fcf02d4a480371aaa03287c6
ndy15 <sshandi08@gmail.com>
2023-05-25 23:09:02 +0800

    initial cm

---
+++ script.js
@@ -0,0 +1,17 @@
+// let body = document.body
+// let darkMode = document.getElementById("dark-mode")
+// let header = document.getElementById("header")
+// let section = document.getElementById("tea")
+// let about = document.getElementById("about")
+// let button = document.querySelector("button")
+// let logo = document.querySelector(".logo")
+// let list = document.getElementsByTagName(".list > a")
+
+// darkMode.addEventListener('click', () => {
+//   darkMode.classList.toggle('active')
+//   header.classList.toggle('active')
+//   section.classList.toggle('active')
+//   about.classList.toggle('active')
+//   button.classList.toggle('active')
+//   logo.classList.toggle('active')
+// })
